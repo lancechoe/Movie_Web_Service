fe200ac3920abfb89c648c506211a5a5f6bdecbd
Lance Choe <musd2839@gmail.com>
2025-05-29 22:13:50 +0900

    try5

--- react-for-beginner/src/App.js
+++ react-for-beginner/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import Detail from "./routes/Detail";
 import "./App.module.css";
