af2651b4587da381d9fd268ee6d3280851ff0a74
Lance Choe <musd2839@gmail.com>
2025-05-29 14:21:23 +0900

    Added Details

--- react-for-beginner/src/routes/Detail.js
+++ react-for-beginner/src/routes/Detail.js
@@ -1,4 +1,45 @@
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+
 function Detail() {
-  return <h1>Detail</h1>;
+  const { id } = useParams();
+  const [loading, setLoading] = useState(true);
+  const [movie, setMovie] = useState(null);
+
+  const getMovies = async () => {
+    const response = await fetch(
+      "https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year"
+    );
+    const json = await response.json();
+    const found = json.data.movies.find((movie) => movie.id === parseInt(id));
+    setMovie(found);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getMovies();
+  }, [id]);
+
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : movie ? (
+        <div>
+          <h1>{movie.title}</h1>
+          <img src={movie.medium_cover_image} alt={movie.title} />
+          <p>{movie.summary || "No summary available."}</p>
+          <ul>
+            {movie.genres.map((g) => (
+              <li key={g}>{g}</li>
+            ))}
+          </ul>
+        </div>
+      ) : (
+        <p>Movie not found.</p>
+      )}
+    </div>
+  );
 }
+
 export default Detail;
--- react-for-beginner/src/routes/Home.js
+++ react-for-beginner/src/routes/Home.js
@@ -113,6 +113,7 @@ function Home() {
             </>
           )}
         </h4>
+        <hr />
       </div>
 
       {/* Movies */}
@@ -124,9 +125,9 @@ function Home() {
             {movies.map((movie) => (
               <Movie
                 key={movie.id}
+                id={movie.id}
                 coverImg={movie.medium_cover_image}
                 title={movie.title}
-                summary={movie.summary}
                 genres={movie.genres}
               />
             ))}
