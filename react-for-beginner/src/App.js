da8416a32c162f2ce5abe80c9e63b0077836bf91
Lance Choe <musd2839@gmail.com>
2025-05-31 19:52:36 +0900

    다시 HashRouter

--- react-for-beginner/src/App.js
+++ react-for-beginner/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import Detail from "./routes/Detail";
 import "./App.module.css";
@@ -7,8 +7,8 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />}></Route>
-        <Route path="/movie/:id" element={<Detail />}></Route>
+        <Route path="/" element={<Home />} />
+        <Route path="/movie/:id" element={<Detail />} />
       </Routes>
     </Router>
   );
